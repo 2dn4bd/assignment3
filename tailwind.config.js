dfa3259d318eadf365de7eefe30d62887bd550ea
2dn4bd <sohelmahmud047@gmail.com>
2023-07-28 00:22:09 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['*'],
+    theme: {
+      screen: {
+        sm:"425px"
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
